f57ac4f7dcd3e7ae0474efd69fb4970fd72c4373
DoHuuThuong <88819021+DoHuuThuong@users.noreply.github.com>
2024-08-29 22:34:03 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from code_runner import run_code_in_docker
 from test_cases import get_test_cases
 from streamlit_monaco import st_monaco
 
-# Kiểm tra kết nối với Docker
+# Thiết lập Docker host
+docker_host = 'npipe:////./pipe/docker_engine'  # Đối với Windows
+# docker_host = 'unix:///var/run/docker.sock'  # Đối với Unix/Linux
+
 try:
-    client = docker.DockerClient(base_url='npipe:////./pipe/docker_engine')
-    print(client.containers.list())
+    client = docker.DockerClient(base_url=docker_host)
+    st.write("Docker is connected and running.")
 except docker.errors.DockerException as e:
-    print(f"Error connecting to Docker: {e}")
+    st.error(f"Error connecting to Docker: {e}")
+    st.stop()  # Dừng ứng dụng Streamlit nếu không kết nối được Docker
 
 language = st.selectbox("Select Language", ["Java", "C"])
-print(language.lower())
 content = st_monaco(
     value="// First line\nfunction hello() {\n\talert('Hello world!');\n}\n// Last line",
     height="200px",
@@ -24,16 +27,13 @@ content = st_monaco(
 
 if st.button("Run Code"):
     test_cases = get_test_cases(language)
-    
     passed_tests = 0
     total_tests = len(test_cases)
 
     for test_input, expected_output in test_cases:
-        # Prepend the test input to the code if needed (e.g., input via arguments)
         full_code = content  # Modify this if test_input should be part of the code
-        
         output = run_code_in_docker(full_code, language)
-        
+
         if output.strip() == expected_output.strip():
             st.success(f"Test Passed: Input: {test_input}")
             passed_tests += 1
